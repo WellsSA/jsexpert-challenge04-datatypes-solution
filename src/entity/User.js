fb631118aa79ee18623beda50f6532d606614e7f
WellsSA <wel.cavzod@gmail.com>
2022-04-07 04:58:00 -0300

    feat(entity): solving User

--- src/entity/User.js
+++ src/entity/User.js
@@ -3,6 +3,17 @@ class User {
     this.id = id;
   }
   // TODO: Alguma manipulação a fazer considerando que o objeto gerado será constantemente escrito em tela?
+  // Não chega a ser usado na aplicação, mas está aqui para fins didáticos :)
+  get [Symbol.toStringTag]() {
+    return 'USER';
+  }
+
+  // TODO: Alguma manipulação a fazer considerando que o objeto gerado será constantemente escrito em tela?
+  [Symbol.toPrimitive](coercionType) {
+    if (coercionType !== 'string') throw new TypeError();
+
+    return `[id=${this.id}]`;
+  }
 }
 
 export default User;
