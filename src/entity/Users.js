7eb3d3817ded205bf81b36fcedb128bdda7bd641
WellsSA <wel.cavzod@gmail.com>
2022-04-07 04:58:11 -0300

    feat(entity): solving Users

--- src/entity/Users.js
+++ src/entity/Users.js
@@ -1,22 +1,30 @@
 import User from './User.js';
 
 // TODO: Criar um Symbol para a propriedade privada 'kUsers'
+const kUsers = Symbol('kUsers');
 class Users {
   constructor() {
-    // TODO: inicializar a propriedade privada 'kData' como uma estrutura importante vista no curso
+    // TODO: inicializar a propriedade privada 'kUsers' como uma estrutura importante vista no curso
+    this[kUsers] = new Set();
   }
 
   add(userRaw) {
     const user = new User(userRaw);
     // TODO: inserir valor na estrutura escolhida.
+    this[kUsers].add(new User(user));
   }
 
   hasUsers() {
     // TODO: Como saber se tem informação dentro da estrutura escolhida?
-    return false;
+    return this[kUsers].size > 0;
   }
 
   // TODO: Me parece que o objeto gerado precisa ser iterável 🤔
+  *[Symbol.iterator]() {
+    for (const user of this[kUsers]) {
+      yield user;
+    }
+  }
 }
 
 export default Users;
